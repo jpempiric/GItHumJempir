ca81051fd735d7bb0956e97aa0159d2bf9dd2f05
jagdish <abc.gmail.com>
2024-06-26 15:54:59 +0530

    Modified Class

---
+++ znewProjectForGit/src/gitHubProject1Package/GitHubProject1Class.java
@@ -0,0 +1,12 @@
+package gitHubProject1Package;
+
+public class GitHubProject1Class {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHubProject1 Class");
+		System.out.println("Hellow Updated Git");
+		System.out.println("Hellow Deleting Git");
+
+	}
+
+}
