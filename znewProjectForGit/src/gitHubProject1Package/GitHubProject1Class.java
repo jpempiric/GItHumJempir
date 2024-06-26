acdfa9c9b38e9f5c4f9f659f4f788d17d1b6a38e
jagdish <abc.gmail.com>
2024-06-26 15:56:01 +0530

    Deleted code

--- znewProjectForGit/src/gitHubProject1Package/GitHubProject1Class.java
+++ znewProjectForGit/src/gitHubProject1Package/GitHubProject1Class.java
@@ -5,7 +5,7 @@ public class GitHubProject1Class {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHubProject1 Class");
 		System.out.println("Hellow Updated Git");
-		System.out.println("Hellow Deleting Git");
+		
 
 	}
 
